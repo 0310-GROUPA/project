5b61eeb7132b8a0aa8264163db2b92d8fa301600
Ilya Vasilev <54356735+Linoliumer@users.noreply.github.com>
2021-03-26 17:29:22 +0300

    Upgrade

--- N-2/NZER_N_B.cpp
+++ N-2/NZER_N_B.cpp
@@ -2,8 +2,9 @@
 #include "NZER_N_B.h"
 #include <vector>
 /*Проверка на ноль*/
-bool NZER_N_B(NaturalNumber n) {
-	if (n.arrayNum[0] == 0 && n.olderCoef == 1) { /*Если первый разряд равен нулю, и размер равен 1, тогда возвращаем false*/
+bool NZER_N_B(NaturalNumber &number) {
+	if (number.arrayNum[0] == 0 && number.olderCoef == 1) {
+	/*Если первый разряд равен нулю, и размер равен 1, тогда возвращаем false*/
 		return false;
 	}
 	return true;
