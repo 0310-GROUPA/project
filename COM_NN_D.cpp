028ebbcfdd2851a7a1d43b686380ce107e38ec67
Sergej Sokolov <81110233+cra3ybrazy@users.noreply.github.com>
2021-03-26 15:20:25 +0300

    Update COM_NN_D.cpp

--- COM_NN_D.cpp
+++ COM_NN_D.cpp
@@ -1,26 +1,20 @@
-
 #include <iostream>
 #include "COM_NN_D.h"
-#include <vector>
 
 using namespace std;
 
-int COM_NN_D(NaturalNumber a, NaturalNumber b) { //a,b - массивы введенных чисел. last1,last2 - первые числа старших разрядов 
+int COM_NN_D(NaturalNumber a, NaturalNumber b) { //a,b - массивы введенных чисел
 	int res, res2, i, razr;
 
-	for (i = 0, razr = 1, res = 0; i < a.olderCoef; i++) {
-		res = res + a.arrayNum[i] * razr;
+	for (i = 0,razr = 1, res = 0; i < a.olderCoef; i++) {
+		res += a.arrayNum[i] * razr;
 		razr *= 10;
-	}
-	cout << res; //Перекладывание первого массива в число
+	}  //Перекладывание первого массива в число
 
 	for (i = 0, razr = 1, res2 = 0; i < b.olderCoef; i++) {
-		res2 = res2 + b.arrayNum[i] * razr;
+		res2 += b.arrayNum[i] * razr;
 		razr *= 10;
 	} //Перекладывание  второго массива в число
-
-
-	cout << " " << res2;
 
 	if (res > res2) return 2;
 	else if (res == res2) return 0;
@@ -28,4 +22,5 @@ int COM_NN_D(NaturalNumber a, NaturalNumber b) { //a,b - массивы введ
 }
 /*Сравнение натуральных чисел.
 Выполнил Соколов Сергей.*/
+
 
