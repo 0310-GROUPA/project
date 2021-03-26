6926a11e50432dc583631738a2889de998f0e7a3
Sergej Sokolov <81110233+cra3ybrazy@users.noreply.github.com>
2021-03-26 15:22:50 +0300

    Update COM_NN_D.cpp

--- COM_NN_D.cpp
+++ COM_NN_D.cpp
@@ -3,17 +3,14 @@
 
 int COM_NN_D(NaturalNumber a, NaturalNumber b) { //a,b - массивы введенных чисел
 	int res, res2, i, razr;
-
 	for (i = 0,razr = 1, res = 0; i < a.olderCoef; i++) {
 		res += a.arrayNum[i] * razr;
 		razr *= 10;
 	}  //Перекладывание первого массива в число
-
 	for (i = 0, razr = 1, res2 = 0; i < b.olderCoef; i++) {
 		res2 += b.arrayNum[i] * razr;
 		razr *= 10;
 	} //Перекладывание  второго массива в число
-
 	if (res > res2) return 2;
 	else if (res == res2) return 0;
 	else return 1;
