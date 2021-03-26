2be474f630f38f1cba8d89c15a58173d53a5d6af
Sergej Sokolov <81110233+cra3ybrazy@users.noreply.github.com>
2021-03-26 16:21:24 +0300

    Update COM_NN_D.cpp

--- COM_NN_D.cpp
+++ COM_NN_D.cpp
@@ -2,14 +2,14 @@
 #include "COM_NN_D.h"
 
 int COM_NN_D(NaturalNumber a, NaturalNumber b) { //a,b - массивы введенных чисел
-	int i, razr;
-	unsigned long long res, res2;
+	int i;
+	unsigned long long res, res2, razr;
 	for (i = 0, razr = 1, res = 0; i < a.olderCoef; i++) {
-		res += a.arrayNum[i] * razr;
+		res += static_cast<__int64>(a.arrayNum[i]) * razr;
 		razr *= 10;
 	}  //Перекладывание первого массива в число
 	for (i = 0, razr = 1, res2 = 0; i < b.olderCoef; i++) {
-		res2 += b.arrayNum[i] * razr;
+		res2 += static_cast<__int64>(b.arrayNum[i]) * razr;
 		razr *= 10;
 	} //Перекладывание  второго массива в число
 	if (res > res2) return 2;
@@ -18,5 +18,4 @@ int COM_NN_D(NaturalNumber a, NaturalNumber b) { //a,b - массивы введ
 }
 /*Сравнение натуральных чисел.
 Выполнил Соколов Сергей.*/
-
 
