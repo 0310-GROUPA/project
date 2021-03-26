078adccf354fd6a23be06eeab36552c50b3a1858
Sergej Sokolov <81110233+cra3ybrazy@users.noreply.github.com>
2021-03-26 17:07:40 +0300

    Update ADD_1N_N.cpp

--- N-3/ADD_1N_N.cpp
+++ N-3/ADD_1N_N.cpp
@@ -5,7 +5,7 @@ unsigned long long ADD_1N_N(NaturalNumber a) {
 	int i;
 	unsigned long long res, razr;
 	for (i = 0, razr = 1, res = 0; i < a.olderCoef; i++) {
-		res += (a.arrayNum[i]) * razr; 
+		res += a.arrayNum[i] * razr; 
 		razr *= 10;
 	} //converting an array to a number
 	res += 1; //adding 1
