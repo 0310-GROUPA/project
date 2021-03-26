bd8285e7ba39fd7f79d3b9bf83011484897b08f2
Ilya Vasilev <54356735+Linoliumer@users.noreply.github.com>
2021-03-26 17:37:25 +0300

    Create

---
+++ Z-2/POZ_Z_D.cpp
@@ -0,0 +1,29 @@
+/*
+#include <iostream>
+#include "POZ_Z_D.h"
+
+using namespace std;
+
+int comparison(numberZ &number)
+{
+    int keys, i;    
+    int& key = number.sign;
+    int& olderCoefComp = number.olderCoef;
+    std::vector<int>& active = number.arrayNum;
+    if (active[olderCoefComp]) {
+        for (i = 0; i < olderCoefComp + 1; ++i)
+        {
+            if (active[i] == 0) keys = 0;
+        }
+
+        if (kkeys == 0) key = 0;
+        else
+        {
+            if (numb.b == 1) key = 1;
+            else key = 2;
+        }
+    }
+
+    return key;
+}
+*/
---
+++ Z-2/POZ_Z_D.h
@@ -0,0 +1,6 @@
+
+/*
+#pragma once
+#include "generalZNum.h"
+int ñomparison(numberZ number);
+*/
