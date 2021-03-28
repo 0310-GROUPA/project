12aa6ad317cde4508aaab8a0b941542190903642
Ilya Vasilev <54356735+Linoliumer@users.noreply.github.com>
2021-03-28 16:00:11 +0300

    Update COM_NN_D.cpp

--- N-1/COM_NN_D.cpp
+++ N-1/COM_NN_D.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include "COM_NN_D.h"
 
-int COM_NN_D(NaturalNumber &a, NaturalNumber &b) {
+int COM_NN_D(NaturalNumber& a, NaturalNumber& b) {
 	int i;
-	unsigned long long res, res2, razr;
-	for (i = 0, razr = 1, res = 0; i < a.olderCoef; i++) {
-		res += a.arrayNum[i] * razr;
-		razr *= 10;
-	}  //Swapping the first array into a number
-	for (i = 0, razr = 1, res2 = 0; i < b.olderCoef; i++) {
-		res2 += b.arrayNum[i] * razr;
-		razr *= 10;
-	} //Swapping the second array into a number
-	if (res > res2) return 2;
-	else if (res == res2) return 0;
-	else return 1; //Comparison
+	if (a.olderCoef > b.olderCoef) return 2;
+	else if (a.olderCoef > b.olderCoef) return 1;
+	else {
+		for (i = a.olderCoef - 1; i > 0; i--) {
+			if (a.arrayNum[i] > b.arrayNum[i]) return 2;
+			else if (a.arrayNum[i] < b.arrayNum[i]) return 1;
+
+		}
+		return 0;
+	}
+	//Comparison
 }
 /*Comparison of natural numbers
-Created by Sergej Sokolov*/
+Completed by Sergej Sokolov*/
