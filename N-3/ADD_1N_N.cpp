780af02528531165f1d11b22a34fe33ae559b5e1
Ilya Vasilev <54356735+Linoliumer@users.noreply.github.com>
2021-04-02 03:41:04 +0300

    Update ADD_1N_N.cpp

--- N-3/ADD_1N_N.cpp
+++ N-3/ADD_1N_N.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include "ADD_1N_N.h"
 
-NaturalNumber ADD_1N_N(NaturalNumber &number) {
+void ADD_1N_N(NaturalNumber &number) {
 	int i,r;
-	NaturalNumber result;
-	result.arrayNum.resize(number.olderCoef);
-	result.olderCoef = number.olderCoef;
 	r = 1;
 	for (i = 0; i < number.olderCoef; ++i) {
-		result.arrayNum[i] = (number.arrayNum[i] + r) % 10;
+		number.arrayNum[i] = (number.arrayNum[i] + r) % 10;
 		r = int((number.arrayNum[i] + r) / 10);
 	}
 	if (r) {
-		result.arrayNum.resize(result.olderCoef + 1);
-		++result.olderCoef;
-		result.arrayNum[result.olderCoef - 1] = r;
+		number.arrayNum.resize(number.olderCoef + 1);
+		++number.olderCoef;
+		number.arrayNum[number.olderCoef - 1] = r;
 	}
-	return result;
 	
 }
 
