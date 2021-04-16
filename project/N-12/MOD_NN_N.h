5ebe502629c99efff58718713fc58be4f378c628
Ilya Vasilev <54356735+Linoliumer@users.noreply.github.com>
2021-04-16 23:13:17 +0300

    Delete project directory

--- project/N-12/MOD_NN_N.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "DIV_NN_N.h"
-#include "SUB_NDN_N.h"
-#include "generalNatNum.h"
-
-NaturalNumber MOD_NN_N(NaturalNumber& number1, NaturalNumber& number2);
--- project/N-7/MUL_Nk_N.h
+++
@@ -1,5 +0,0 @@
-
-#pragma once
-#include "generalNatNum.h"
-
-NaturalNumber MUL_Nk_N(NaturalNumber& number, int degree);
