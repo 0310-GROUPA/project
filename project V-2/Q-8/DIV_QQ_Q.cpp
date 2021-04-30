afadfb239eb9ace02e6874b4bb501632db74e07c
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:29:12 +0300

    Update DIV_QQ_Q.cpp

--- project V-2/Q-8/DIV_QQ_Q.cpp
+++ project V-2/Q-8/DIV_QQ_Q.cpp
@@ -4,12 +4,12 @@ numberR DIV_QQ_Q(numberR R1, numberR R2) {
 	numberR res;
 	numberZ numerator;
 	NaturalNumber denominator;
-	numerator = TRANS_N_Z(R2.natural);
-	res.whole = MUL_ZZ_Z(R1.whole, numerator); //calculate the numerator
+	numerator = TRANS_N_Z(R2.natural);  //!
+	res.whole = MUL_ZZ_Z(R1.whole, numerator); //calculate the numerator   //!
 	denominator = TRANS_Z_N(R2.whole);
-	res.natural = MUL_NN_N(R1.natural, denominator); //calculate the denominator
+	res.natural = MUL_NN_N(R1.natural, denominator); //calculate the denominator  //!
 	if (R2.whole.sign == 1) {
-		res.whole = MUL_ZM_Z(res.whole); //voiding losing sign with natural multiplication
+		res.whole = MUL_ZM_Z(res.whole); //voiding losing sign with natural multiplication  //!
 	}
 
 	return res;
