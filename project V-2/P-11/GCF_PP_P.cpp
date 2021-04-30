8c1befafef2a5229cc60424ef5432d59f870ca8a
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:20:03 +0300

    Update GCF_PP_P.cpp

--- project V-2/P-11/GCF_PP_P.cpp
+++ project V-2/P-11/GCF_PP_P.cpp
@@ -6,7 +6,7 @@ polinom GCF_PP_P(polinom P1, polinom P2)
     polinom result, temp, tempS, tempT, elsetemp , polinom1, polinom2;
     polinom1 = P1;
     polinom2 = P2;
-    temp = MOD_PP_P(polinom1,polinom2);
+    temp = MOD_PP_P(polinom1,polinom2);  //!
     if(!((temp.olderCoef==0)&&(XarrayZtoX(temp.arrayNum[0].whole) == 0))){
         tempS =MOD_PP_P(polinom2,temp);
         if(!((tempS.olderCoef==0)&&(XarrayZtoX(tempS.arrayNum[0].whole) == 0))){
