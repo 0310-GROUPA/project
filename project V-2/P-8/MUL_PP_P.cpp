e7a668043d1f94c7be845365adbea8f0b829914b
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:17:51 +0300

    Update MUL_PP_P.cpp

--- project V-2/P-8/MUL_PP_P.cpp
+++ project V-2/P-8/MUL_PP_P.cpp
@@ -8,9 +8,9 @@ polinom MUL_PP_P(polinom P1,polinom P2){
     result.olderCoef = 0;
     result.arrayNum.push_back(XtoRArrayX(zero, one));
     for(i=P1.olderCoef;i>=0;--i){
-        temp = MUL_Pxk_P(P2, i);
-        temp = MUL_PQ_P(temp,P1.arrayNum[i]);
-        result = ADD_PP_P(result, temp);
+        temp = MUL_Pxk_P(P2, i);               //!
+        temp = MUL_PQ_P(temp,P1.arrayNum[i]);  //!
+        result = ADD_PP_P(result, temp);       //!
     }
     correct(result);
     return result;
