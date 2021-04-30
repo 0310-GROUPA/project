ba7cb6de46127df4c923cb20eb8ca036b08b4fa4
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:19:16 +0300

    Update MOD_PP_P.cpp

--- project V-2/P-10/MOD_PP_P.cpp
+++ project V-2/P-10/MOD_PP_P.cpp
@@ -5,9 +5,9 @@
 polinom MOD_PP_P(polinom poly1, polinom poly2)
 {
     polinom result;
-    result = DIV_PP_P(poly1, poly2);
+    result = DIV_PP_P(poly1, poly2);  //!
     correct(result);
-    result = MUL_PP_P(result, poly2);
-    result = SUB_PP_P(poly1, result);
+    result = MUL_PP_P(result, poly2); //!
+    result = SUB_PP_P(poly1, result); //!
     return result;
 }
