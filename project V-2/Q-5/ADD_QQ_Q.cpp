074cece53a7e3ad3e71a6ca7a1d1d1ecef4f1dd4
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:25:55 +0300

    Update ADD_QQ_Q.cpp

--- project V-2/Q-5/ADD_QQ_Q.cpp
+++ project V-2/Q-5/ADD_QQ_Q.cpp
@@ -8,19 +8,19 @@ numberR ADD_QQ_Q(numberR first, numberR second)
     numberZ first_lcm, second_lcm;
     numberZ firstWhole, seconWhole;
     if(XarrayZtoX(first.whole) == 0){
-        return RED_Q_Q(second);
+        return RED_Q_Q(second);                         //!
     }else if (XarrayZtoX(second.whole) == 0){
         return RED_Q_Q(first);
     }else{
-        lcm = LCM_NN_N(first.natural, second.natural);
+        lcm = LCM_NN_N(first.natural, second.natural);  //!
         res.natural = lcm;
-        temp = DIV_NN_N(lcm, first.natural);
-        first_lcm = TRANS_N_Z(temp);
+        temp = DIV_NN_N(lcm, first.natural);            //!
+        first_lcm = TRANS_N_Z(temp);                    //!
         temp = DIV_NN_N(lcm, second.natural);
         second_lcm = TRANS_N_Z(temp);
-        firstWhole = MUL_ZZ_Z(first_lcm, first.whole);
+        firstWhole = MUL_ZZ_Z(first_lcm, first.whole);  //!
         seconWhole = MUL_ZZ_Z(second_lcm, second.whole);
-        res.whole = ADD_ZZ_Z(firstWhole, seconWhole);
+        res.whole = ADD_ZZ_Z(firstWhole, seconWhole);   //!
         res = RED_Q_Q(res);
         return res;
     }
