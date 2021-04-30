57b902dfc3123330f4527168600e89b4c0381669
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:21:30 +0300

    Update NMR_P_P.cpp

--- project V-2/P-13/NMR_P_P.cpp
+++ project V-2/P-13/NMR_P_P.cpp
@@ -3,10 +3,10 @@
 numberR NMR_P_P(polinom P){
     numberR res;
 
-    res = FAC_P_Q(DIV_PP_P(P,GCF_PP_P(P,DER_P_P(P))));
+    res = FAC_P_Q(DIV_PP_P(P,GCF_PP_P(P,DER_P_P(P))));  //!
 //find the gcd of the polynomial and its derivative, divide the polynomial by this gcd,
 // then derivation the LCM of the denominators of the coefficients and the GCD of the numerators from the resulting polynomial
-    res = RED_Q_Q(res);
+    res = RED_Q_Q(res); //!
     return res;
 }
 /*Created by Gorshenin Aleksandr*/
