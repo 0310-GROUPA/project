b6afdde2df4fdba0ecd4820aff2f2cfd1ecea733
Frolkova Nastya <81257954+TutaTamta@users.noreply.github.com>
2021-04-30 16:07:46 +0300

    Update MOD_NN_N.cpp

--- project V-2/N-12/MOD_NN_N.cpp
+++ project V-2/N-12/MOD_NN_N.cpp
@@ -12,8 +12,8 @@ NaturalNumber MOD_NN_N(NaturalNumber& number1, NaturalNumber &number2) {
 	'2' - the first is greater than the second.
 	'1' - the second is greater than the first.
 	'0' - are equal. */
-    if(!NZER_N_B(number1)) return XtoXArray(zero);
-	flagCom = COM_NN_D(number1, number2);
+    if(!NZER_N_B(number1)) return XtoXArray(zero);  //!
+	flagCom = COM_NN_D(number1, number2);       //!
 	/* If two numbers are equal, then the remainder of their division is '0' */
     if (flagCom == 0) return XtoXArray(zero);
 	/* If the second is larger than the first,
@@ -21,12 +21,12 @@ NaturalNumber MOD_NN_N(NaturalNumber& number1, NaturalNumber &number2) {
 	else if (flagCom == 1) return MOD_NN_N(number2, number1);
 	else {
 		/* Calculation of the quotient from dividing the first number by the second. */
-		temp2 = DIV_NN_N(number1, number2);
+		temp2 = DIV_NN_N(number1, number2);  //!
 		/* Multiplication of the quotient by the second number. */
-		temp = MUL_NN_N(number2,temp2);
+		temp = MUL_NN_N(number2,temp2);      //!
 		/* Subtracting the first number from the result from the second item. */
-		result = SUB_NN_N(number1, temp);
+		result = SUB_NN_N(number1, temp);    //!
 		return result;
 	}
 }
-/* Created by Vasilev Ilya */
+/* Created by Gorshenin Aleksandr */
